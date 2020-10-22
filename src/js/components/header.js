ef3e13cc0a6825a104f481f95f6dbc617e4ab24b
coatriver <sylvain.jacot@hotmail.be>
2020-10-22 07:52:03 +0200

    gsap and barba added

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -7,34 +7,8 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-        <style>
-          nav {
-            height: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color:  #0a0a23;
-          }
-          
-          ul li {
-            list-style: none;
-            display: inline;
-          }
-          
-          a {
-            font-weight: 700;
-            margin: 0 25px;
-            color: #fff;
-            text-decoration: none;
-          }
-          
-          a:hover {
-            padding-bottom: 5px;
-            box-shadow: inset 0 -2px 0 0 #fff;
-          }
-        </style>
         <header>
-          <nav>
+          <nav class="mainNavBar">
             <ul>
               <li><a href="./index.html">HomePage</a></li>
               <li><a href="./about.html">About</a></li>
--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,6 @@ function callAll(jsfiles) {
   src.setAttribute("src", jsfiles);
   document.getElementsByTagName("body")[0].appendChild(src);
 }
-
+callAll("./src/js/scripts/barba.js");
+callAll("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js");
 callAll("./src/js/components/header.js");
